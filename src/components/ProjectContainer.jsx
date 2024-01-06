56cf9656958d0565a5f1d6fa843167b38ce36740
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-05 16:13:12 -0800

    Break project section into seperate container

---
+++ src/components/ProjectContainer.jsx
@@ -0,0 +1,61 @@
+import "./styles/ProjectContainer.css";
+
+function ProjectContainer({ reversed, project }) {
+    {
+        if (reversed) {
+            return (
+                <div className="projectContainer reversedProjectContainer">
+                    <div className="projectDetails">
+                        <h3 className="projectTitle">{project.title}</h3>
+                        <div className="projectTags">
+                            {project.tags.map((tag) => {
+                                return <p className="projectTag">{tag}</p>;
+                            })}
+                        </div>
+                        <p className="projectInfo">{project.description}</p>
+                        <div className="projectLinks">
+                            <a href="" className="projectLink">
+                                Review the Code
+                            </a>
+                            <a href="" className="projectLink">
+                                See it Live
+                            </a>
+                        </div>
+                    </div>
+                    <div className="projectImages">
+                        <div className="projectImage">Project Image</div>
+                        <div className="projectImage">Project Image</div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="projectContainer">
+                    <div className="projectImages">
+                        <div className="projectImage">Project Image</div>
+                        <div className="projectImage">Project Image</div>
+                    </div>
+                    <div className="projectDetails">
+                        <h3 className="projectTitle">{project.title}</h3>
+                        <div className="projectTags">
+                            {project.tags.map((tag) => {
+                                return <p className="projectTag">{tag}</p>;
+                            })}
+                        </div>
+                        <p className="projectInfo">{project.description}</p>
+                        <div className="projectLinks">
+                            <a href="" className="projectLink">
+                                Review the Code
+                            </a>
+                            <a href="" className="projectLink">
+                                See it Live
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+
+export default ProjectContainer;
