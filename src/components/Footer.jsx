f18c851a37b1a5c41cf6d77d1de61049c929d36b
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-05 18:57:04 -0800

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,10 @@ function Footer() {
     return (
         <footer className="footer">
             <div>Developed by Patrick Brebner</div>
+            <div>|</div>
+            <div>
+                Copyright <i class="fa-regular fa-copyright"></i> 2023
+            </div>
         </footer>
     );
 }
