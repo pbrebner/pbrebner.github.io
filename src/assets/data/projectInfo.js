98029d093507f4fcc94e6a920456c60183eb63cc
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-24 16:54:42 -0800

    Add project info

--- src/assets/data/projectInfo.js
+++ src/assets/data/projectInfo.js
@@ -1,26 +1,43 @@
+import blogOne from "./projectImages/blogOne.png";
+import blogTwo from "./projectImages/blogTwo.png";
+
 import membersOne from "./projectImages/membersOne.png";
 import membersTwo from "./projectImages/membersTwo.png";
+
+import weatherOne from "./projectImages/weatherOne.png";
+import weatherTwo from "./projectImages/weatherTwo.png";
 
 // Project Info List: Secondary Image and links are optional (Everything else is required)
 const projectList = [
     {
+        mainImage: blogOne,
+        secondaryImage: blogTwo,
+        title: "Blog Website",
+        tags: ["NodeJs", "Passport"],
+        description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos magni fugit eius quasi assumenda odit quidem sunt, explicabo amet modi quia dolor facere dolores error earum sit adipisci mollitia officia.",
+        website: "https://pbrebner.github.io/blog-client/",
+        github: "https://github.com/pbrebner/blog-client",
+    },
+    {
         mainImage: membersOne,
         secondaryImage: membersTwo,
         title: "Members Only Club",
-        tags: ["React", "NodeJs"],
+        tags: ["NodeJs", "Pug/Jade", "Passport"],
         description:
             "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos magni fugit eius quasi assumenda odit quidem sunt, explicabo amet modi quia dolor facere dolores error earum sit adipisci mollitia officia.",
         website: "https://members-only-deploy.fly.dev/",
         github: "https://github.com/pbrebner/members-only",
     },
     {
-        mainImage: membersOne,
-        title: "Blog Website",
-        tags: ["React", "NodeJs"],
+        mainImage: weatherOne,
+        secondaryImage: weatherTwo,
+        title: "Weather App",
+        tags: ["Javascript", "Webpack", "API"],
         description:
             "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos magni fugit eius quasi assumenda odit quidem sunt, explicabo amet modi quia dolor facere dolores error earum sit adipisci mollitia officia.",
-        website: "https://members-only-deploy.fly.dev/",
-        github: "",
+        website: "https://pbrebner.github.io/weather-app/",
+        github: "https://github.com/pbrebner/weather-app",
     },
 ];
 
