67e39c2d95dc5fe94541e9e447ed54d481ca5d98
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-27 17:37:56 -0800

    Update project info

--- src/assets/data/projectInfo.js
+++ src/assets/data/projectInfo.js
@@ -7,13 +7,27 @@ import membersTwo from "./projectImages/membersTwo.png";
 import weatherOne from "./projectImages/weatherOne.png";
 import weatherTwo from "./projectImages/weatherTwo.png";
 
+import imageOne from "./projectImages/imageClassifierOne.png";
+import imageTwo from "./projectImages/imageClassifierTwo.png";
+import imageThree from "./projectImages/imageClassifierThree.png";
+
+import textOne from "./projectImages/textClassifierOne.png";
+
 // Project Info List: Secondary Image and links are optional (Everything else is required)
 const projectList = [
     {
         mainImage: blogOne,
         secondaryImage: blogTwo,
         title: "Blog Website",
-        tags: ["React", "AWS S3", "NodeJs", "Express", "MongoDB", "JWT"],
+        tags: [
+            "React",
+            "AWS S3",
+            "NodeJs",
+            "Express",
+            "MongoDB",
+            "JWT",
+            "Restful API Design",
+        ],
         description:
             "Fully featured blog with backend API integration. Users are able to sign-up, manage their profiles, view and post content, and interact with others through comments and likes.",
         website: "https://pbrebner.github.io/blog-client/",
@@ -38,6 +52,25 @@ const projectList = [
             "A weather forecast application that utilizes the open weather API to display relevant data of the searched city on a clean user interface.",
         website: "https://pbrebner.github.io/weather-app/",
         github: "https://github.com/pbrebner/weather-app",
+    },
+    {
+        mainImage: imageThree,
+        secondaryImage: imageTwo,
+        title: "Image Classifier with PyTorch",
+        tags: ["Python", "PyTorch", "Computer Vision"],
+        description:
+            "Built a convolutional neural network using PyTorch for image classification of the CIFAR-10 dataset. Presented the accuracy as a function of training epoch, analyzing the effects of activation function, layer configuration and data augmentation on performance vs overfitting.",
+        website: "",
+        github: "https://github.com/pbrebner/image-classifier-with-pytorch",
+    },
+    {
+        mainImage: textOne,
+        title: "Text Classifier with Scikit-Learn",
+        tags: ["Python", "Scikit-Learn", "Machine Learning"],
+        description:
+            "Built and tested the text classification performance of multiple machine learning algorithms. Implemented text normalization and developed a model validation pipeling using k-fold cross validation.",
+        website: "",
+        github: "https://github.com/pbrebner/text-classifier-with-scikit-learn",
     },
 ];
 
